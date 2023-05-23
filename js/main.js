6f9c3a4bd2db843274607dd058641ff1f082eb23
xIMet3 <joaquin19_91@hotmail.com>
2023-05-23 22:57:01 +0200

    I modify some css styles and the main file to create the random color scheme without success. Added possible styles for victory messages and replay button commented in the css file. // Modifico algunos stilos del css y el archivo main para crear la combinacion aleatoria de colores sin exito. Añado posibles estilos para mensajes de victoria y boton de volver a jugar comentados en el archivo css.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   let colores = JSON.parse(sessionStorage.getItem('colores123456'));
   let combinacionSecreta = document.querySelector('.combsecreta');
-  let colorUno = combinacionSecreta.querySelector('.color-a1');
-  let colorDos = combinacionSecreta.querySelector('.color-a2');
-  let colorTres = combinacionSecreta.querySelector('.color-a3');
-  let colorCuatro = combinacionSecreta.querySelector('.color-a4');
+  let colorUno = combinacionSecreta.querySelector('.color-uno');
+  let colorDos = combinacionSecreta.querySelector('.color-dos');
+  let colorTres = combinacionSecreta.querySelector('.color-tres');
+  let colorCuatro = combinacionSecreta.querySelector('.color-cuatro');
 
   let elementosColor = [colorUno, colorDos, colorTres, colorCuatro];
 
   for (let i = 0; i < elementosColor.length; i++) {
-    elementosColor[i].style.backgroundColor = colores[i].color;
+    let indiceColorAleatorio = generarNumeroAleatorio(0, colores.length);
+    elementosColor[i].style.background = colores[indiceColorAleatorio].color;
+  }
+
+  function generarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
   }
 });
