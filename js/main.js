09136bea11fa5b8f8544c8e0b0571b1f503c1a12
xIMet3 <joaquin19_91@hotmail.com>
2023-05-24 20:53:33 +0200

    I get the board to pick up the colors of the session storage and make background in the circles of the secret combination with a random combination. I relink the js to the html since it was wrongly linked. I change the elements that belong to the 4 circles of the secret combination so that they are called the same. // Consigo que el tablero recoja los colores del session storage y haga background en los circulos de la combinacion secreta con una combinacion aleatoria. Vuelvo linkear el js al html ya que estaba mal linkeado. Cambio los elementos que pertenecen a los 4 circulos de la combinacion secreta para que se llamen igual.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let colores = JSON.parse(sessionStorage.getItem('colores123456'));
-  let combinacionSecreta = document.querySelector('.combsecreta');
-  let colorUno = combinacionSecreta.querySelector('.color-uno');
-  let colorDos = combinacionSecreta.querySelector('.color-dos');
-  let colorTres = combinacionSecreta.querySelector('.color-tres');
-  let colorCuatro = combinacionSecreta.querySelector('.color-cuatro');
+// Recibimos el array de colores del sessionStorage, esto debería devolver solo un array con los color
+let colores = JSON.parse(sessionStorage.getItem('colores123456'));
+console.log(colores);
 
-  let elementosColor = [colorUno, colorDos, colorTres, colorCuatro];
-
-  for (let i = 0; i < elementosColor.length; i++) {
-    let indiceColorAleatorio = generarNumeroAleatorio(0, colores.length);
-    elementosColor[i].style.background = colores[indiceColorAleatorio].color;
-  }
-
-  function generarNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+// Esto te lo puedes cargar cuando devulvas solo el array de colores
+let coloresArray = colores.map((color) => {
+  return color.color;
 });
+// Aquí creas la combinacion secreta.
+let combinacionSecreta = [];
+for(let i = 0; i < coloresArray.length; i++) {
+  let randomIndex = Math.floor( Math.random() * coloresArray.length);
+  combinacionSecreta.push(coloresArray[randomIndex]);
+}
+// Este imprime el array de los colores disponibles
+console.log("coloresArray", coloresArray);
+// Esto imprime el array de la combinacion secreta
+console.log("combinacionSecreta", combinacionSecreta);
+// Esto son todos los elemetons 'span' de '.combsecreta'
+let coloresSecretosHTMLElements = document.querySelectorAll('.combsecreta .color-secreto');
+// Lo pasa desde NodeList a Array
+let coloresSecretosHTMLElementsArray = Array.from(coloresSecretosHTMLElements);
+// imprime un array con los elementos HTML de los colores secretos
+console.log(coloresSecretosHTMLElementsArray);
+// Cambia el color de los elementos de la combinacion secreta
+coloresSecretosHTMLElementsArray.map((spanColor, contador) => {
+  spanColor.style.backgroundColor = coloresArray[contador];
+})
