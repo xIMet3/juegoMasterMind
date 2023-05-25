710d4558e23fc543455d8e67434628df991c31f0
xIMet3 <joaquin19_91@hotmail.com>
2023-05-25 20:45:00 +0200

    I change the class names of the first row by row-1 to all. I get that when clicking on the buttons of the colors the elements of the row-1 are painted in order and that when arriving at the last one it stops. I will change the names of the classes of all the rows by row-2, row-3... to go painting row by row. // Cambio los nombres de la clase de la primera fila por fila-1 a todos. Consigo que al pulsar en los botones de los colores se pinten en orden los elementos de la fila-1 y que al llegar al ultimo se pare. Cambiare los nombres de las clases de todas las filas por fila-2, fila-3..  para ir pintando fila por fila.

--- js/main.js
+++ js/main.js
@@ -23,4 +23,36 @@ coloresSeleccionados.forEach((spanColor, index) => {
   spanColor.style.backgroundColor = colores[index];
 });
 
+const botones = document.querySelectorAll('.color-seleccionado');
+const elementosFila1 = document.querySelectorAll('.fila-1');
+const elementosPintados = new Set();
+let indiceElementoActual = 0;
+
+botones.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    const colorSeleccionado = boton.style.backgroundColor;
+
+    // Verificar si el elemento actual ya ha sido pintado
+    if (elementosPintados.has(elementosFila1[indiceElementoActual])) {
+      return; // Salir de la función si el elemento ya ha sido pintado
+    }
+
+    // Pintar el elemento actual con el color seleccionado
+    elementosFila1[indiceElementoActual].style.backgroundColor = colorSeleccionado;
+
+    // Agregar el elemento actual al conjunto de elementos pintados
+    elementosPintados.add(elementosFila1[indiceElementoActual]);
+
+    // Incrementar el índice del elemento actual
+    indiceElementoActual++;
+
+    // Verificar si se ha pintado todos los elementos
+    if (indiceElementoActual === elementosFila1.length) {
+      // Aquí puedes realizar alguna acción adicional si se han pintado todos los elementos
+      console.log('Se han pintado todos los elementos de fila-1');
+    }
+  });
+});
+
+
 
