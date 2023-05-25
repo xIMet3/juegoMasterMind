d3c8344197ad6246308fd7f9fec4c4ec887498a7
xIMet3 <joaquin19_91@hotmail.com>
2023-05-25 21:03:41 +0200

    I give function to the 'delete' button to delete the colors of each row in order. // Doy funcion al boton 'borrar' para que borre los colores de cada fila en orden.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-// Recibimos el array de colores del sessionStorage, esto debería devolver solo un array con los colores
+// COMBINACION SECRETA ALEATORIA
+// Recoge el array de colores del sessionStorage.
 let colores = JSON.parse(sessionStorage.getItem('colores123456'));
-// Aquí creas la combinacion secreta.
+// Crea la combinacion aleatoria.
 let combinacionSecreta = [];
 for(let i = 0; i < colores.length; i++) {
   let randomIndex = Math.floor( Math.random() * colores.length);
   combinacionSecreta.push(colores[randomIndex]);
 }
-
-// Esto son todos los elementos 'span' de '.combsecreta'
+// BOTONES CON LOS COLORES
+// Esto son todos los elementos span de .combsecreta
 let coloresSecretosHTMLElements = document.querySelectorAll('.combsecreta .color-secreto');
 // Lo pasamos de NodeList a Array
 let coloresSecretosHTMLElementsArray = Array.from(coloresSecretosHTMLElements);
@@ -16,13 +17,14 @@ coloresSecretosHTMLElementsArray.forEach((spanColor, index) => {
   spanColor.style.backgroundColor = combinacionSecreta[index];
 });
 
-// Esto son cada uno de los elementos HTML de los colores seleccionados
+// Esto son cada uno de los elementos HTML de los colores seleccionados.
 const coloresSeleccionados = document.querySelectorAll(".color-seleccionado");
 // Cambia el color de los elementos de los botones
 coloresSeleccionados.forEach((spanColor, index) => {
   spanColor.style.backgroundColor = colores[index];
 });
-
+// BOTON BORRAR
+// Funcion del boton borrar.
 const botones = document.querySelectorAll('.color-seleccionado');
 const elementosFila1 = document.querySelectorAll('.fila-1');
 const elementosPintados = new Set();
@@ -32,27 +34,33 @@ botones.forEach((boton) => {
   boton.addEventListener('click', () => {
     const colorSeleccionado = boton.style.backgroundColor;
 
-    // Verificar si el elemento actual ya ha sido pintado
     if (elementosPintados.has(elementosFila1[indiceElementoActual])) {
-      return; // Salir de la función si el elemento ya ha sido pintado
+      return;
     }
 
-    // Pintar el elemento actual con el color seleccionado
     elementosFila1[indiceElementoActual].style.backgroundColor = colorSeleccionado;
-
-    // Agregar el elemento actual al conjunto de elementos pintados
     elementosPintados.add(elementosFila1[indiceElementoActual]);
 
-    // Incrementar el índice del elemento actual
     indiceElementoActual++;
 
-    // Verificar si se ha pintado todos los elementos
     if (indiceElementoActual === elementosFila1.length) {
-      // Aquí puedes realizar alguna acción adicional si se han pintado todos los elementos
       console.log('Se han pintado todos los elementos de fila-1');
     }
   });
 });
+
+const botonBorrar = document.querySelector('.borrar');
+
+botonBorrar.addEventListener('click', () => {
+  if (elementosPintados.size > 0) {
+    const ultimoElementoPintado = elementosFila1[indiceElementoActual - 1];
+    ultimoElementoPintado.style.backgroundColor = '';
+
+    elementosPintados.delete(ultimoElementoPintado);
+    indiceElementoActual--;
+  }
+});
+
 
 
 
